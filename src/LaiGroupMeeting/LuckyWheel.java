79877a49447eb37570d3207b80d0f2e32bba56d1
MasterOfAlll <lu286@ucsb.edu>
2021-06-19 01:18:28 -0700

    add

--- src/LaiGroupMeeting/LuckyWheel.java
+++ src/LaiGroupMeeting/LuckyWheel.java
@@ -1,37 +1,72 @@
 package LaiGroupMeeting;
 
+import java.util.*;
+
 import java.util.Random;
 
 public class LuckyWheel {
 
-    public static void main(String[] args) {
-        Random rand = new Random();
-        System.out.println(rand.nextInt(9));
-        System.out.println(rand.nextInt(9));
-        System.out.println(rand.nextInt(9));
+    public static void main(String[] args){
+
+        String[] names = new String[] {
+                "Tim",
+                "Boxuan",
+                "Zeyu",
+                "Youqi",
+                "Gaoyuan",
+                "Yuewei",
+                "Jingyi",
+                "Wensi"//,
+                //"Wenxuan"
+        };
+
+        printGrouping(grouping(names));
     }
 
-    static String[] pick(String[] array, int num) {
-        Random rand = new Random();
-        String[] result = new String[num];
-        int counter = 0;
-        while(counter < num) {
-            int randNum = rand.nextInt(array.length - counter);
-            result[counter] = array[randNum];
-            swap(array, randNum, array.length-1-counter);
-            counter++;
+    public static void printGrouping(List<List<String>> groups) {
+
+        System.out.println("Coding Practice Groups For This Week\n");
+
+        for(int i=0; i<groups.size(); i++) {
+            System.out.println("Group " + (i+1));
+            System.out.println("-------");
+            for(String name : groups.get(i))
+                System.out.print(name + " ");
+            System.out.println("\n");
         }
-        return result;
     }
 
-    static boolean strIsNum(String str) {
-        String regex = "^[0-9]+$";
-        return str.matches(regex);
+    public static List<List<String>> grouping(String[] names) {
+
+        List<List<String>> groups = new ArrayList<>();
+
+        int left = names.length;
+        Scanner scanner = new Scanner(System.in);
+
+        while(left>0) {
+            System.out.println(left + " people are left for grouping.\n");
+            List<String> group = new ArrayList<>();
+            System.out.print("Enter group size for group " + (groups.size()+1) + ": ");
+            int count = Math.min(left, Math.max(1, scanner.nextInt()));
+            while(count>0) {
+                int who = new Random().nextInt(left);
+                group.add(names[who]);
+                swap(names, who, --left);
+                count--;
+            }
+            System.out.println();
+            groups.add(group);
+        }
+
+        System.out.println();
+        return groups;
     }
 
-    static void swap(String[] array, int i, int j) {
-        String tmp = array[i];
-        array[i] = array[j];
-        array[j] = tmp;
+    private static void swap(String[] names, int a, int b) {
+        String temp = names[a];
+        names[a] = names[b];
+        names[b] = temp;
     }
+
 }
+
